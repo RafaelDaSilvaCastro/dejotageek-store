3ed21b6186a4eeece3eacfba4978b0ba06c2e365
Guilherme Brito Pizzollo <86429792+guilhermebp030504@users.noreply.github.com>
2023-10-10 20:51:39 -0300

    Update ProdutoController.java

--- Backend/src/main/java/br/com/dejota/dejotaApi/Controller/ProdutoController.java
+++ Backend/src/main/java/br/com/dejota/dejotaApi/Controller/ProdutoController.java
@@ -1,10 +1,11 @@
 package br.com.dejota.dejotaApi.Controller;
 
 import br.com.dejota.dejotaApi.Modelo.Produto;
-import br.com.dejota.dejotaApi.Modelo.Usuario;
 import br.com.dejota.dejotaApi.repositorio.ProdutoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produto")
@@ -30,7 +31,35 @@ public class ProdutoController {
 
     }
 
+    @GetMapping("/nome/{nome_produto}")
+    public List<Produto> buscaProdutoPorNome(@PathVariable String nome_produto){
+        return prodRepositorio.findByNome(nome_produto);
 
+    }
+
+    @GetMapping("/descricao/{descricao_produto}")
+    public List<Produto> buscaProdutoPorDescricao(@PathVariable String nome_descricao){
+        return prodRepositorio.findByDescricaoLike(nome_descricao);
+
+    }
+
+    @GetMapping("/preco/{valor1}/{valor2}")
+    public List<Produto> buscaProdutoPorPreco(@PathVariable Double valor1, @PathVariable Double valor2){
+        return prodRepositorio.findFaixaPreco(valor1, valor2);
+
+    }
+
+    @GetMapping("/estoque/{valor1}/{valor2}")
+    public List<Produto> buscaProdutoPorEstoque(@PathVariable Integer valor1, @PathVariable Integer valor2){
+        return prodRepositorio.findFaixaEstoque(valor1, valor2);
+
+    }
+
+    @GetMapping("/tudo")
+    public List<Produto> buscaProdutoTodos(){
+        return prodRepositorio.findAllProduto();
+
+    }
 
 
 
