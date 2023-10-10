7aac39a2d9322342a7fc10711dc6d391d864e57b
Guilherme Brito Pizzollo <86429792+guilhermebp030504@users.noreply.github.com>
2023-10-10 20:52:02 -0300

    Update ProdutoRepositorio.java

--- Backend/src/main/java/br/com/dejota/dejotaApi/repositorio/ProdutoRepositorio.java
+++ Backend/src/main/java/br/com/dejota/dejotaApi/repositorio/ProdutoRepositorio.java
@@ -6,8 +6,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
 
     @Query("SELECT COUNT(*) FROM Produto p WHERE p.id_produto = :idProduto")
     Boolean existsByid_produto(@Param("idProduto") Long idProduto);
+
+    List<Produto> findByNome(String produto);
+
+    List<Produto> findByDescricaoLike(String produto);
+
+    @Query(value = "SELECT * FROM Produto p WHERE p.preco between :Valor1 AND :Valor2", nativeQuery = true)
+    List<Produto> findFaixaPreco(@Param("Valor1") Double valor1, @Param("Valor2") Double valor2);
+
+    @Query(value = "SELECT * FROM Produto p WHERE p.estoque between :Valor1 AND :Valor2", nativeQuery = true)
+    List<Produto> findFaixaEstoque(@Param("Valor1") Integer valor1, @Param("Valor2") Integer valor2);
+
+    @Query(value = "SELECT * FROM Produto", nativeQuery = true)
+    List<Produto> findAllProduto();
 }
