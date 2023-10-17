577fcb959179c731be2d453f38a75b2eb518ff1a
Gabriel Canarin Salazar <86989100+GabrielCanarin@users.noreply.github.com>
2023-10-17 20:01:01 -0300

    Filtro API

--- Frontend/src/componentes/FiltroDashboard.jsx
+++ Frontend/src/componentes/FiltroDashboard.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function FiltroDashboard(props) {
+
+  const handleInicioDataChange = (e) => {
+    const novadataInicio = e.target.value;
+    props.setDataInicio(novadataInicio);
+  };
+
+  const handleFimDataChange = (e) => {
+    const novadataFim = e.target.value;
+    props.setDataFim(novadataFim);
+  };
+
+  const handleCategoriaChange = (e) => {
+    const novaCategoria = e.target.value;
+    props.setCategoria(novaCategoria);
+  };
+
   return (
     <div className="flex items-center text-cinza-claro gap-7">
       <div className="flex items-center gap-2 ">
@@ -9,9 +25,9 @@ function FiltroDashboard(props) {
         <input
           className=" rounded-lg p-2 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] outline-none w-48 "
           type="date"
-          id="date"
-          required
+          id="dateInicio"
           placeholder="De"
+          onChange={handleInicioDataChange}
         />
       </div>
       <div className="flex items-center gap-2">
@@ -20,26 +36,23 @@ function FiltroDashboard(props) {
         <input
           className=" rounded-lg p-2 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] outline-none w-48 "
           type="date"
-          id="date"
-          required
-          placeholder="De"
+          id="dateFim"
+          placeholder="Ate"
+          onChange={handleFimDataChange}
         />
       </div>
       <div className="flex items-center">
         <select
           id="categoria"
           name="Categoria"
-          required
           className="  outline-none drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] rounded-lg h-10 p-2 bg-white text-cinza-claro w-64 "
+          onChange={handleCategoriaChange}
         >
           {props.categoriafiltro && props.categoriafiltro.map((categoria) => (
             <option className="text-zinc-800" value={categoria}>
               {categoria}
             </option>
           ))}
-
-
-
         </select>
       </div>
     </div>
