343d5504e51cb52b85fdce9f9cbb97c5548d8e82
Gabriel Canarin Salazar <86989100+GabrielCanarin@users.noreply.github.com>
2023-11-15 19:46:18 -0300

    compra e venda quase completos

--- Frontend/src/Pages/CompraEVenda/CompraEVenda.jsx
+++ Frontend/src/Pages/CompraEVenda/CompraEVenda.jsx
@@ -1,4 +1,4 @@
-import CardVazio from "../../componentes/CardVazio";
+import Card from "../../componentes/Card";
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import blogFetch from "../../axios/config";
@@ -6,54 +6,102 @@ import blogFetch from "../../axios/config";
 function CompraEVenda() {
   const [selectedRadio, setSelectedRadio] = useState("Compra");
 
-  //referente a opção Compra e Venda
   const handleRadioChange = (value) => {
     setSelectedRadio(value);
   };
 
-  const [formularioAtivo, setFormularioAtivo] = useState("Compra");
-  // 'selecionar se é o form de compra ou venda'
+  const [posts, setPosts] = useState([]);
 
-  //form
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/produto/todos");
+      const data = response.data;
+      setPosts(data);
+      setNomes(data.nome);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const [nome, setNome] = useState("");
   const [data, setData] = useState("");
   const [precoUnitario, setprecoUnitario] = useState(0);
+  const [preco, setPreco] = useState(0);
   const [quantidade, setQuantidade] = useState(0);
-  const [imagem, setImagem] = useState(null);
+  const [imagem, setImagem] = useState("../../public/assets/imagem-vazia.png");
+  const [codigo, setCodigo] = useState(null);
+  const [estoque, setEstoque] = useState(null);
+  const dataAtual = new Date();
 
-  const receberImagem = (imagem) => {
-    setImagem(imagem);
-  };
+  const handleVar = (post) => {
+    setNome(post.nome);
+    setImagem(post.imagem);
+    setCodigo(post.id_produto);
+    setQuantidade(0);
+    setPreco(post.preco);
+    setEstoque(post.estoque);
+    setData(dataAtual.toLocaleDateString());
+  }
 
-  const valorTotal = (precoUnitario, quantidade) => {
+  const valorCompra = (precoUnitario, quantidade) => {
     return precoUnitario * quantidade;
   };
 
-  const limparDados = () => {
-    console.log(nome, data, precoUnitario, quantidade);
+  const valorVenda = (preco, quantidade) => {
+    return preco * quantidade;
+  };
 
+  const limparDados = () => {
     setNome("");
-    setData("");
-    setprecoUnitario(0);
+    setImagem("../../public/assets/imagem-vazia.png");
+    setCodigo(null);
     setQuantidade(0);
-    console.log(nome, data, precoUnitario, quantidade);
+    setPreco(0);
+    setprecoUnitario(0)
+    setEstoque(0);
+    setData("");
+  };
+
+
+  const form = {
+    TipoTransação: selectedRadio,
+    id_produto: codigo,
+    ValorVenda: preco,
+    precoCompra: precoUnitario,
+    data: data,
+    estoque: estoque,
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const form = {
-      imagem,
-      nome,
-      data,
-      precoUnitario,
-      quantidade,
-    };
-    console.log(form);
-    try {
-      await blogFetch.post("/CompraVenda", { body: form });
-    } catch (error) {
-      console.log("error");
-      alert("Não foi possível conectar !!");
+
+    if (quantidade > estoque && selectedRadio == "Venda") {
+      alert("Quantidade maior que a disponivel no estoque(${estoque})")
+    }
+    else {
+      try {
+        console.log(form);
+        await blogFetch.post(
+          "/transacao",
+          form
+        );
+        console.log(form);
+        if (selectedRadio == "Venda") {
+          alert("Venda cadastrada!");
+        }
+        else {
+          alert("Compra cadastrada!");
+        }
+
+      } catch (error) {
+        console.error(error);
+        console.log(form);
+        alert("Não foi possível conectar!!");
+      }
     }
   };
 
@@ -67,8 +115,7 @@ function CompraEVenda() {
               : "text-black w-36 text-xl p-2"
           }
           onClick={() =>
-            handleRadioChange("Compra") && setFormularioAtivo("Compra")
-          }
+            handleRadioChange("Compra")}
         >
           Compra
         </button>
@@ -80,18 +127,17 @@ function CompraEVenda() {
               : "text-black w-36 text-xl p-2"
           }
           onClick={() =>
-            handleRadioChange("Venda") && setFormularioAtivo("Venda")
-          }
+            handleRadioChange("Venda")}
         >
           Venda
         </button>
       </div>
 
       <div className="flex justify-around mr-28 mb-8 mt-12 items-center ml-20">
-        <CardVazio
+        <Card
           nome={nome}
-          valorTotal={valorTotal(quantidade, precoUnitario)}
-          enviarVariavelImg={receberImagem}
+          preco={preco}
+          imagem={imagem}
         />
         <div className="h-barraCinza w-px mr-4 bg-cinza-claro"></div>
         <form
@@ -103,40 +149,45 @@ function CompraEVenda() {
             Nome
           </label>
           <input
-            className=" outline-none p-2 rounded-lg w-96 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] placeholder:text-cinza-claro"
+            className="outline-none p-2 rounded-lg w-96 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] placeholder:text-cinza-claro"
             placeholder="Nome"
             type="text"
             name="nome"
             id="nome"
+            list="listaItens"
             required
             onChange={(e) => setNome(e.target.value)}
           />
-          <label
-            htmlFor="date"
-            className="flex justify-between items-center text-cinza-claro pl-2"
-          >
-            <p>Data</p>
-            <input
-              className=" rounded-lg p-2 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] outline-none w-60 "
-              type="date"
-              id="date"
-              required
-              onChange={(e) => setData(e.target.value)}
-            />
-          </label>
-          <label
-            htmlFor="precoUnitario"
-            className="flex justify-between items-center text-cinza-claro pl-2"
-          >
-            <p>Preço Unitário</p>
-            <input
-              className=" rounded-lg p-2 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] outline-none w-60 "
-              type="number"
-              id="precoUnitario"
-              required
-              onChange={(e) => setprecoUnitario(e.target.value)}
-            />
-          </label>
+
+          <datalist id="listaItens">
+            {posts.map(item => (
+              <option
+                key={item.id_produto}
+                value={item.nome}
+                onClick={() => handleVar(item)}
+              >
+                {item.nome}
+              </option>
+            ))}
+          </datalist>
+
+          {selectedRadio == 'Compra' ? (
+            <label
+              htmlFor="precoUnitario"
+              className="flex justify-between items-center text-cinza-claro pl-2"
+            >
+              <p>Preço Unitário</p>
+              <input
+                className=" rounded-lg p-2 h-10 drop-shadow-[0px_3px_7px_rgba(0,0,0,0.25)] outline-none w-60 "
+                type="number"
+                id="precoUnitario"
+                required
+                onChange={(e) => setprecoUnitario(e.target.value)}
+              />
+            </label>
+          ) : null}
+
+
           <label
             htmlFor="quantidade"
             className="flex justify-between items-center text-cinza-claro pl-2"
@@ -152,8 +203,18 @@ function CompraEVenda() {
           </label>
           <div className="h-0.5 w-48 bg-cinza-claro ml-44"></div>
           <div className=" flex  justify-between items-center text-cinza-claro pl-2 pr-20">
-            <p>Total</p>
-            <p>R$: {valorTotal(quantidade, precoUnitario)}</p>
+            {selectedRadio == 'Compra' ? (
+              <>
+                <p>Total</p>
+                <p>R$: {valorCompra(precoUnitario, quantidade)}</p>
+              </>
+            ) : (
+              <>
+                <p>Total</p>
+                <p>R$: {valorVenda(preco, quantidade)}</p>
+              </>)}
+
+
           </div>
           <div className="flex justify-between">
             <button
