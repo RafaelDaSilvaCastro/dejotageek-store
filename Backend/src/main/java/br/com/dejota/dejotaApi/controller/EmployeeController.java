60bb34fb5862ca7765ac5716bd74f0689721be63
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2023-11-28 20:51:19 -0300

    Realizando ajustes

--- Backend/src/main/java/br/com/dejota/dejotaApi/controller/EmployeeController.java
+++ Backend/src/main/java/br/com/dejota/dejotaApi/controller/EmployeeController.java
@@ -1,2 +1,24 @@
-package br.com.dejota.dejotaApi.controller;public class EmployeeController {
+package br.com.dejota.dejotaApi.controller;
+
+import br.com.dejota.dejotaApi.dtos.CreateEmployeeDto;
+import br.com.dejota.dejotaApi.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody CreateEmployeeDto dto) {
+        employeeService.save(dto);
+        return ResponseEntity.ok().build();
+    }
 }
